64f3afde48a0229c90705dddd82eb465406c2764
ladthomas <ladouyouthomas@outlook.fr>
2024-11-14 16:34:56 +0100

    modified

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-// Connexion à la base de données SQLite
+// Connexion SQLite
 const db = new sqlite3.Database(path.join(__dirname, 'database.sqlite'), (err) => {
     if (err) {
         console.error('Erreur lors de l\'ouverture de la base de données', err.message);
--- server/server.js
+++ server/server.js
@@ -6,17 +6,16 @@ const studentRoutes = require('./routes/students');
 
 const app = express();
 
-//  CORS pour autoriser les requêtes cross-origin
-app.use(cors()); // Ceci permet à toutes les origines d'accéder à l'API
+//  CORS 
+app.use(cors()); 
 
-//  bodyParser pour parser les JSON dans les requêtes
 app.use(bodyParser.json());
 
-// Routes pour les classrooms et les students
+// Rote  les classrooms et les students
 app.use('/api/classrooms', classroomRoutes);
 app.use('/api/classrooms', studentRoutes);
 
-// Démarrer le serveur
+
 app.listen(3000, () => {
     console.log('Serveur démarré sur le port 3000');
 });
